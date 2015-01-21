553a0ef51ca3d4fbd8600432d46f2d9ac0c35b04
Tejas Lagvankar <advantej@yahoo-inc.com>
2015-01-20 18:09:28 -0800

    Initial commit

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -9,7 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *billTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+- (void) updateValues;
 @end
 
 @implementation ViewController
@@ -22,6 +26,26 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+
+- (void)updateValues {
+    float billAmount = [self.billTextField.text floatValue];
+    
+    NSArray *tipValues = @[@(0.1), @(0.2), @(0.3)];
+    float tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex] floatValue];
+    
+    float totalFloatAmount = tipAmount + billAmount;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"%f", tipAmount];
+    self.totalLabel.text = [NSString stringWithFormat:@"%f", totalFloatAmount];
+    
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:true];
+    [self updateValues];
 }
 
 @end
